c0e467900b9af518ef970a104b2416f5253f0570
Khadija Tul Kubra <49521166+khadijatulkubra1998@users.noreply.github.com>
2019-07-15 09:46:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+let fiveStarHotel = {
+    name: "Mövenpick Hotel Karachi",
+    category: "five Star hotel",
+    facilities: ["pool", "free wifi", "free parking", "air condition"],
+    reviews: "79",
+    ratings: "5",
+    nearbyAreas: ["flag staff house", "fere hotel", "karachi airport"],
+    aboutThisPropert: {
+        name: "Mövenpick Hotel Karachi",
+        detail: "This hotel features 5 restaurants, an outdoor pool, and a coffee shop/café. Free WiFi in public areas and free valet parking are also provided. Additionally, an outdoor tennis court, a spa tub, and a sauna are onsite. All 407 rooms offer free WiFi, 24-hour room service, and LCD TVs. Minibars, coffee makers, and free newspapers are among the other amenities available to guests.",
+    },
+    roomInformation: [
+        classicRoom1KingBed: {
+            bedDetails: "1 king bed",
+            sleeps: {
+                maxPeople: "3",
+                maxAduls: "3",
+                maxChildren: "2"
+            },
+            roomDetail: "1 King Bed 377-sq-foot (35-sq-meter) room, balcony with city view",
+            internet: "free WiFi",
+            foodAndDrink: ["Coffee/tea maker", "minibar", "24-hour room service", "free bottled water"],
+            sleep: "Turndown service",
+            bathroom: ["Private bathroom", "shower", " free toiletries", "a hair dryer"],
+            practical: ["Safe", "free newspaper", "desk", "rollaway/extra beds", "free cribs/infant beds available on request"],
+            comfort: ["Air conditioning ", "daily housekeeping", "Smoking ", " Non-Smoking"],
+            option1: ["price $115 (per night)", "Non-Refundable", "Free WiFi", "Free valet parking"],
+            option2: ["price $135 (pernight)", "Non-Refundable", "Free WiFi", "Free Breakfast", "Free valet parking"],
+        },
+        classicRoom2TwinBeds: {
+            bedDetails: "2 twin bed",
+            sleeps: {
+                maxPeople: "3",
+                maxAduls: "3",
+                maxChildren: "2",
+            },
+            roomDetails: "2 Twin Beds 377-sq-foot (35-sq-meter) room, balcony with city views",
+            internet: "Free WiFi",
+            entertainment: " LCD television",
+            foodAndDrink: [" Coffee/tea maker", "minibar", "24-hour room service", "free bottled water"],
+            sleep: "Turndown service",
+            bathroom: ["Private bathroom", "shower", " free toiletries", "a hair dryer"],
+            practical: ["Safe", "free newspaper", "desk", "rollaway/extra beds", "free cribs/infant beds available on request"],
+            comfort: ["Air conditioning ", "daily housekeeping", "Smoking ", " Non-Smoking"],
+            option1: ["price $115 (per night)", "Non-Refundable", "Free WiFi", "Free valet parking"],
+            option2: ["price $135 (pernight)", "Non-Refundable", "Free WiFi", "Free Breakfast", "Free valet parking"],
+        }
+    ],
+    aboutThisArea: [
+        karachi: "Located in Karachi, Mövenpick Hotel Karachi is in the city center. Frere Hall and Holy Trinity Cathedral are local landmarks, and the area's natural beauty can be seen at Clifton Beach and Beach Park. Looking to enjoy an event or a game while in town? See what's happening at Hockey Club of Pakistan or DHA Stadium. Our guests appreciate the hotel's central location.",
+        whatsNearby: {
+            flagStaffHouse: " 10 min walk",
+            frereHall: "14 min walk",
+            holyTrinityCathedral: "15 min walk",
+            nationalMuseumofPakistan: "18 min walk",
+            sindhHighCourt: " 3 min drive",
+        },
+        gettingAround: "Karachi (KHI-Jinnah Intl.) - 13 min drive",
+        resturants: {
+            thePakistani: ["Restaurant offering Pakistani cuisine", "Serves dinne"],
+            alBustan: ["Restaurant offering international cuisine", "Serves breakfast", "lunch", "dinner."],
+            kababJi: ["Restaurant offering Middle Eastern cuisine", "Serves dinner"],
+            laMamma: ["Restaurant offering Italian cuisine", "Serves lunch", "dinner"],
+            fanoosLounge: ["Cafe", "Serves light fare", "Open 24 hours"],
+        }
+    ],
+    
+    propertyAmenities: {
+        internet: {
+            rooms: "Available in all rooms: Free WiFi",
+            publicPlace: "Available in some public areas: Free WiFi"
+        },
+
+    }
+}
